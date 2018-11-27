8cd0ccb1c02f86b895ba8ac190da032c8ad7dac3
Ben Scott <gamepoet@gmail.com>
2018-11-26 21:41:27 -0800

    gracefully handle freeing NULL

--- src/frag.cpp
+++ src/frag.cpp
@@ -182,6 +182,10 @@ void* allocator_alloc(frag_allocator_t* allocator, size_t size, size_t alignment
 }
 
 void allocator_free(frag_allocator_t* allocator, void* ptr, const char* file, int line, const char* func) {
+  if (ptr == NULL) {
+    return;
+  }
+
   // protect access to this allocator if necessary
   optional_lock_guard_t lock((std::mutex*)allocator->mutex);
 
